3aceeaf9fe8d691926b7c57aa0abc90f383d4eb0
James Oh <jamesboh@gmail.com>
2017-03-14 14:27:30 -0400

    update header

--- browser/components/Header.js
+++ browser/components/Header.js
@@ -5,7 +5,20 @@ export default class Header extends Component {
 	render () {
 		return (
 			<div id="header">
-				<h1>Header</h1>
+				<div id="logo">
+					<h1>SpartaPlaesent</h1>
+				</div>
+				<div id="nav">
+					<ul>
+						<li>212.555.5555</li>
+						<li>LOGIN</li>
+					</ul>
+				</div>
+				<div id="hamburger">
+					<div></div>
+					<div></div>
+					<div></div>
+				</div>
 			</div>
 		);
 	}
