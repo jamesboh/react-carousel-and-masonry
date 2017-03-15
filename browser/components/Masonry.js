f3601eb9e78e0b30d4fbc9b03f9395935cc84597
James Oh <jamesboh@gmail.com>
2017-03-14 23:31:18 -0400

    more masonry layout adjustments

--- browser/components/Masonry.js
+++ browser/components/Masonry.js
@@ -45,7 +45,7 @@ export default class Masonry extends Component {
 	}
 
 	componentDidMount () {
-		this.handleImageLoaded();
+		// this.handleImageLoaded();
 		window.addEventListener('resize', this.handleImageLoaded);
 	}
 
@@ -54,47 +54,51 @@ export default class Masonry extends Component {
 	}
 
 	handleImageLoaded () {
-		//media queries
-		let columns;
-		let gutter;
-		let totalContentWidth;
-		if (window.innerWidth <= 576) {
-			columns = 1;
-			gutter = 0;
-			totalContentWidth = 100;
-		} else if (window.innerWidth <= 768) {
-			columns = 2;
-			gutter = 10;
-			totalContentWidth = 90;
-		} else {
-			columns = 3;
-			gutter = 5;
-			totalContentWidth = 90;
-		}
-
-		const articlesDOM = [...document.getElementsByTagName('article')];
-
-		//initialize width
-		articlesDOM.forEach( article => article.style.width = '' + totalContentWidth/columns + '%' );
-
-		//object to store position from top with columns as keys
-		let positionsFromTop = {};
-		for ( let i = 0; i < columns; i++ ) {
-			positionsFromTop[i] = 0;
-		}
-		
-		for ( let i = 0; i < articlesDOM.length; i++ ) {
-			//set horizontal positions
-			articlesDOM[i].style.left = '' + (totalContentWidth/columns + gutter) * (i%columns) + '%';
-			
-			//set vertical positions
-			if ( i < columns ) {
-				articlesDOM[i].style.top = positionsFromTop[i];
+		setTimeout(() => { //set timeout is to address a positioning bug with the 2nd column 2nd row item.
+			//media queries
+			let columns;
+			let gutter;
+			let totalContentWidth;
+			let marginBottom = 50;
+			if (window.innerWidth <= 576) {
+				columns = 1;
+				gutter = 0;
+				totalContentWidth = 100;
+			} else if (window.innerWidth <= 768) {
+				columns = 2;
+				gutter = 10;
+				totalContentWidth = 90;
 			} else {
-				positionsFromTop[i%columns] += articlesDOM[i-columns].offsetHeight;
-				articlesDOM[i].style.top = '' + positionsFromTop[i%columns] + 'px';
+				columns = 3;
+				gutter = 5;
+				totalContentWidth = 90;
 			}
-		}
+
+			const articlesDOM = [...document.getElementsByTagName('article')];
+
+			//initialize width
+			articlesDOM.forEach( article => article.style.width = '' + totalContentWidth/columns + '%' );
+
+			//object to store position from top with columns as keys
+			let positionsFromTop = {};
+			for ( let i = 0; i < columns; i++ ) {
+				positionsFromTop[i] = 0;
+			}
+			
+
+			for ( let i = 0; i < articlesDOM.length; i++ ) {
+				//set horizontal positions
+				articlesDOM[i].style.left = '' + (totalContentWidth/columns + gutter) * (i%columns) + '%';
+
+				//set vertical positions
+				if ( i < columns ) {
+					articlesDOM[i].style.top = positionsFromTop[i];
+				} else {
+					positionsFromTop[i%columns] += articlesDOM[i-columns].offsetHeight;
+					articlesDOM[i].style.top = '' + (positionsFromTop[i%columns] + marginBottom) + 'px';
+				}
+			}
+		}, 100)
 	}
 
 	render () {
